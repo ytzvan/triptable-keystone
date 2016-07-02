af51e52cf762b88db91096f03538fbc3aa597c6a
Ytzvan Mastino <mastino14@gmail.com>
2016-07-02 00:37:31 +0000

    Added New Relic License

--- newrelic.js
+++ newrelic.js
@@ -14,7 +14,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: '398a41136434e937b11d714ccd46d8735db36dbd',
+  license_key: process.env.NEWRELIC_LICENSE,
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
