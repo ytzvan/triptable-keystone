c93c7d5ae784d3757b42dd921bf36e428224c8cf
Ytzvan Mastino <mastino14@gmail.com>
2016-07-22 21:57:52 +0000

    added field for supporting alingn vertical home images

--- models/homeGallery.js
+++ models/homeGallery.js
@@ -6,7 +6,8 @@ var homeGallery = new keystone.List('homeGallery');
 homeGallery.add({
   image: { type: Types.CloudinaryImage },
   description : {type: String},
-  url : {type : String}
+  url : {type : String},
+  verticalPosition: {type: Types.Number, default: 50}
 });
 
 homeGallery.register();
