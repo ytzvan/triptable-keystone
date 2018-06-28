4c52a5e788732c0ea6c55ba83d8b33eb9688b736
Ytzvan Mastino <mastino14@gmail.com>
2018-06-28 08:43:53 -0500

    fixed cookie

--- keystone.js
+++ keystone.js
@@ -38,7 +38,7 @@ keystone.init({
 	'signout redirect' : '/',
 	'compress':true,
 	'session options': {    
-		cookie: { secure: false, maxAge: 31104000, domain:'.'+process.env.LOCALDOMAIN,  httpOnly: true  }
+		cookie: { maxAge: 31104000, domain:'.'+process.env.LOCALDOMAIN,  httpOnly: true  }
   },
 //	'sesssion store options' : {cookie: { domain:'.localdomain.com'}}
 		// 'cookie signin options' : { 'maxAge': 5 * 60 * 1000, 'signed': true },
