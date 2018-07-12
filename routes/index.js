2512054f8a1d4028b04f4f35f08b00074e2c62ba
Ytzvan Mastino <mastino14@gmail.com>
2018-07-12 15:01:01 -0500

    disable routes

--- routes/index.js
+++ routes/index.js
@@ -77,8 +77,8 @@ exports = module.exports = function(app) {
 	app.all('/tour/:slug', routes.views.tour);
 
 	//V1 API Routes
-	app.get('/api/tour/list', keystone.middleware.api, routes.api.tour.list);
-	app.get('/api/tour/:id', keystone.middleware.api, routes.api.tour.get);
+//	app.get('/api/tour/list', keystone.middleware.api, routes.api.tour.list);
+//	app.get('/api/tour/:id', keystone.middleware.api, routes.api.tour.get);
 	//Search Views
 	app.get('/:country', routes.search.country);
 	app.get('/:country/:province', routes.search.province);
